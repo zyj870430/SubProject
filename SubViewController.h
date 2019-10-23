41f77169ba3a805cf69199cc0ec0565441e9150b
zyj <zyj>
2019-10-23 09:12:17 +0800

    1

---
+++ SubViewController.h
@@ -0,0 +1,17 @@
+//
+//  SubViewController.h
+//  SubProject
+//
+//  Created by 社会主义接班人 on 2019/10/22.
+//  Copyright © 2019 社会主义接班人. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface SubViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ SubViewController.m
@@ -0,0 +1,33 @@
+//
+//  SubViewController.m
+//  SubProject
+//
+//  Created by 社会主义接班人 on 2019/10/22.
+//  Copyright © 2019 社会主义接班人. All rights reserved.
+//
+
+#import "SubViewController.h"
+
+@interface SubViewController ()
+
+@end
+
+@implementation SubViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    self.title = @"子工程";
+    self.view.backgroundColor = UIColor.whiteColor;
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
